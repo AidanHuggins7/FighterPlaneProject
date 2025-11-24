b3c00246e2a87d68293aa1819d3ff46290781b8b
Aidan Huggins <aidan.lego@bellsouth.net>
2025-11-23 21:36:18 -0500

    Push New Project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject playerPrefab;
-
     public GameObject enemyOnePrefab;
-    public GameObject enemyThreePrefab;
     public GameObject enemyTwoPrefab;
-    public GameObject cloudPrefab;
-    public GameObject healthPrefab;
+    public GameObject enemyThreePrefab;
     public GameObject coinPrefab;
+    public GameObject healthPrefab;
+    public GameObject cloudPrefab;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject powerupPrefab;
+    public GameObject audioPlayer;
+
+    public AudioClip powerupSound;
+    public AudioClip powerdownSound;
 
     public TextMeshProUGUI livesText;
     public TextMeshProUGUI scoreText;
-
+    public TextMeshProUGUI powerupText;
+    
     public float horizontalScreenSize;
     public float verticalScreenSize;
 
     public int score;
+    public int cloudMove;
+
+    private bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +42,8 @@ public class GameManager : MonoBehaviour
         horizontalScreenSize = 10f;
         verticalScreenSize = 6.5f;
         score = 0;
+        cloudMove = 1;
+        gameOver = false;
         AddScore(0);
         Instantiate(playerPrefab, transform.position, Quaternion.identity);
         CreateSky();
@@ -38,12 +52,17 @@ public class GameManager : MonoBehaviour
         InvokeRepeating("CreateSecondEnemy", 2, 3);
         InvokeRepeating("CreateHealth", 1, 5);
         InvokeRepeating("CreateCoin", 1, 7);
+        StartCoroutine(SpawnPowerup());
+        powerupText.text = "No powerups yet!";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     void CreateEnemy()
@@ -71,6 +90,11 @@ public class GameManager : MonoBehaviour
         Instantiate(coinPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.5f, Random.Range(-verticalScreenSize, verticalScreenSize / 2) * 0.5f, 0), Quaternion.identity);
     }
 
+    void CreatePowerup()
+    {
+        Instantiate(powerupPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
+    }
+
     void CreateSky()
     {
         for (int i = 0; i < 30; i++)
@@ -79,19 +103,68 @@ public class GameManager : MonoBehaviour
         }
         
     }
+
+    public void ManagePowerupText(int powerupType)
+    {
+        switch (powerupType)
+        {
+            case 1:
+                powerupText.text = "Speed!";
+                break;
+            case 2:
+                powerupText.text = "Double Weapon!";
+                break;
+            case 3:
+                powerupText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerupText.text = "Shield!";
+                break;
+            default:
+                powerupText.text = "No powerups yet!";
+                break;
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        float spawnTime = Random.Range(3, 5); 
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerup();
+        StartCoroutine(SpawnPowerup());
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerupSound);
+                break;
+            case 2:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerdownSound);
+                break;
+        }
+    }
+
     public void AddScore(int earnedScore)
     {
         score = score + earnedScore;
         scoreText.text = "Score: " + score;
     }
 
-    public void ChangeLivesText(int currentLives)
+    public void ChangeLivesText (int currentLives)
     {
         livesText.text = "Lives: " + currentLives;
     }
 
-    public void ChangeScoreText(int score)
+    public void GameOver()
     {
-        livesText.text = "Score: " + score;
+        gameOverText.SetActive(true);
+        restartText.SetActive(true);
+        gameOver = true;
+        CancelInvoke();
+        cloudMove = 0;
     }
+
 }
